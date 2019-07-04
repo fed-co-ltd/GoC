c550dcb43f99d5efd4847e7c54746c6401ca05ca
Edrian Jose Ferrer <edrianjoseferrer@gmail.com>
2019-07-04 09:55:29 +0800

    Update GoC.cs
    Added Game identifiers

--- Assets/Scripts/GoC.cs
+++ Assets/Scripts/GoC.cs
@@ -1,8 +1,29 @@
 namespace GoC
 {
-    public interface ITroopunit {
+    enum GoCTypes
+    {
+        Hall, EnvironmentFeature, TroopUnit, Resource
+    }
+    public struct TileLocation
+    {
+        int x,y;
+    }
+    public class GObject
+    {
+        protected TileLocation location;
+        protected int Type;
+    }
+
+    public class LObject : GObject
+    {
+        protected int LifePoints;
+        protected int Attack;
+    }
+    public interface ITroopunit 
+    {
         int Attack();
         bool Die();
         int GatherRes();
     }
+
 }
