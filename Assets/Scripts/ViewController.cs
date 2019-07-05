0df2d9f76e7520dd895a92abc5e9a5eda83da2ff
Edrian Jose Ferrer <edrianjoseferrer@gmail.com>
2019-07-06 00:05:58 +0800

    Configure errors in the environment
    unity collab, and google drive issues

---
+++ Assets/Scripts/ViewController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoC;
+
+public class ViewController : MonoBehaviour
+{
+    Camera Cam;
+    Vector2 CamPos;
+    float ScrollPos;
+    public Limit ZoomLimit;
+    public float ZoomingSpeed;
+    
+    void Start()
+    {
+        Cam = gameObject.GetComponent<Camera>();
+        ZoomLimit = new Limit(1,5);
+        CamPos = transform.position;
+        ScrollPos = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var scroll_pos = Input.GetAxis("Mouse ScrollWheel"); // latest scroll position
+        //if (scroll_pos != ScrollPos)
+        //{
+            ChangeZoomSize(scroll_pos);
+        //}
+            
+    }
+
+    void ChangeZoomSize(float scroll_pos)
+    {
+        ScrollPos = scroll_pos;
+        float zoom_size = 0;
+        zoom_size = -scroll_pos * ZoomingSpeed * Time.deltaTime;
+        zoom_size += Cam.orthographicSize;
+        Cam.orthographicSize = Mathf.Clamp(zoom_size, ZoomLimit.x, ZoomLimit.y);
+    }
+}
